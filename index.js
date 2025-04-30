bc2c862ff89fc5d93ff0d1d473f94d939dcbc5bb
MD Sarfaraz <sarfarazm933@gmail.com>
2025-04-30 20:47:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+console.log('Hello jeee, kaise ho saare kiya haal chal');
+
+let a = 5;
+console.log(a);
+
+if(true){
+    let a =5;
+     
+}
+console.log(a);
+
+const num = 12
+console.log(num);
+
+let lastname='khan';
+console.log(lastname)
+
+let firstname='sarfaraz';
+console.log(firstname);
+
+let person = {
+    firstname:'mohd',
+    lastname:'sarfaraz',
+    profession:'software engineer'
+}
+console.log(person);
+
+let x=12
+let y=2
+console.log(x**y);
+
+console.log(3===5);
+
+let age =24;
+let status = (age>=18) ? "Yes Vote" : "Not Vote"
+console.log(status);
+
+let marks = 87
+if(marks>=90){
+    console.log('A')
+}
+else if(marks>=80){
+    console.log('B')
+}
+else if(marks>=70){
+    console.log('C')
+}
+else if(marks>=60){
+    console.log('D')
+}
+else{
+    console.log('E')
+}
+
+let z= 4
+switch(z){
+    case 1: console.log('A')
+    break;
+    case 2: console.log('B')
+    break;
+    case 3: console.log('C')
+    break;
+    case 4: console.log('D')
+    break;
+}
+
+for(let i=0; i<5; i++){
+    console.log(i);
+}
+
+let i=0
+while(i<10){
+    console.log('sarfaraz'+i)
+    i++;
+}
+
+let n=1;
+do{
+    console.log('shaikh'+n)
+    n++;
+}while(n<5);
